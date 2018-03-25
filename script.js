4cbf69d64561355ce9c8a40b2379b0cd48ad0ae3
snakecase <4579891+snakecase@users.noreply.github.com>
2018-03-25 09:54:02 +0500

    Add and remove some comments. Edit some functions

--- script.js
+++ script.js
@@ -1,74 +1,70 @@
-    const computerPlay = function () {
-        const randomNumber = Math.random();
+const computerPlay = function() {
+  const randomNumber = Math.random();
+  return randomNumber <= 0.33
+    ? `rock`
+    : randomNumber > 0.66 ? `paper` : `scissors`;
+};
 
-        return (randomNumber <= 0.33) ? `rock` :
-            (randomNumber > 0.66) ? `paper` : `scissors`;
+const playRound = function(computerPlay, userPlay) {
+  if (userPlay === `canceled`) {
+    return `canceled`;
+  } else if (computerPlay === userPlay) {
+    return `draw`;
+
+    // Check Rock versus ...
+  } else if (computerPlay === `rock`) {
+    return userPlay === `scissors` ? `computer` : `user`;
+
+    // Check Paper versus ...
+  } else if (computerPlay === `paper`) {
+    return userPlay === `rock` ? `computer` : `user`;
+
+    // Check Scissors versus ...
+  } else {
+    return userPlay === `paper` ? `computer` : `user`;
+  }
+};
+
+let computerScore = 0;
+let userScore = 0;
+
+// Invoke playRound each button press then display results in html
+const game = function(e) {
+  // If the paragraph displaying the winner exists then stop the game
+  if (document.querySelector(`.displayResult`)) {
+    return;
+  }
+
+  // Pass pressed button's class as userPlay
+  const roundResult = playRound(computerPlay(), e.target.className);
+
+  const computerDisplayScore = document.querySelector(`.computer-result-p`);
+  const userDisplayScore = document.querySelector(`.user-result-p`);
+
+  // Increment scores and update them in html
+  if (roundResult === `computer`) {
+    computerScore++;
+    computerDisplayScore.textContent = `Computer: ${computerScore}`;
+  } else if (roundResult === `user`) {
+    userScore++;
+    userDisplayScore.textContent = `Player: ${userScore}`;
+  }
+
+  // Append a paragraph to the results block in case any player reaches max score
+  if (computerScore === 5 || userScore === 5) {
+    const divResult = document.querySelector(`.results-div`);
+    const displayResult = document.createElement(`p`);
+    displayResult.className = `displayResult`;
+    divResult.appendChild(displayResult);
+
+    // Declare a winner
+    if (computerScore === 5) {
+      displayResult.textContent = `Computer won!`;
+    } else if (userScore === 5) {
+      displayResult.textContent = `The winner is you! Congratulations!`;
     }
+  }
+};
 
-    const userPlay = function () {
-        let userInput = prompt(`Pick a hand:`);
-
-        // In case user presses Cancel or ESC key
-        if (userInput === null) {
-            return `canceled`;
-        }
-
-        userInput = userInput.toLowerCase();
-        const hands = [`rock`, `paper`, `scissors`];
-
-        if (hands.indexOf(userInput) > -1) {
-            return userInput;
-        } else {
-            // Prompt accepts only legit hand names, otherwise keeps prompting
-            alert(`Please input a correct hand ("Rock", "Paper" or "Scissors")`);
-            return userPlay();
-        }
-    }
-
-    const playRound = function (computerPlay, userPlay) {
-        if (userPlay === `canceled`) {
-            return `canceled`;
-        } else if (computerPlay === userPlay) {
-            return `draw`;
-        }
-        // Check Rock versus ...
-        else if (computerPlay === `rock`) {
-            return (userPlay === `scissors`) ? `computer` : `user`;
-        }
-        // Check Paper versus ...
-        else if (computerPlay === `paper`) {
-            return (userPlay === `rock`) ? `computer` : `user`;
-        }
-        // Check Scissors versus ...
-        else {
-            return (userPlay === `paper`) ? `computer` : `user`;
-        }
-    }
-
-    // Iterates playRound() function ${num} of times,
-    // scores each iteration and returns the winner of the game with final scores
-    const game = function (num) {
-        let computerScore = 0;
-        let userScore = 0;
-
-        for (i = 1; i <= num; i++) {
-            const roundResult = playRound(computerPlay(), userPlay());
-            console.log(`Round ${i}: ${roundResult}`);
-            if (roundResult === `computer`) {
-                computerScore++;
-            } else if (roundResult === `user`) {
-                userScore++;
-            }
-        }
-
-        if (computerScore === userScore) {
-            console.log(`Draw!`);
-            return `draw`;
-        } else if (computerScore > userScore) {
-            console.log(`Computer won with the score of ${computerScore} against ${userScore}!`);
-            return `computer`;
-        } else {
-            console.log(`You won with the score of ${userScore} against ${computerScore}! Congratulations!`);
-            return `user`;
-        }
-    }
+const btns = document.querySelectorAll(`button`);
+btns.forEach(button => addEventListener(`click`, game));
